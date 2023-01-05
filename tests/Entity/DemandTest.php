d791446191755ce2f50235453b0245f5135311f9
HEDHILI <selmen.hedhili89@gmail.com>
2023-01-05 08:46:31 +0100

    feat: documenter et préciser le type de retour de la méthode

--- tests/Entity/DemandTest.php
+++ tests/Entity/DemandTest.php
@@ -7,19 +7,31 @@ use App\Entity\Demand;
 class DemandTest extends \PHPUnit\Framework\TestCase
 {      
            
-    public function testchangeStatusWaitingToInProgress()
+    /**     
+     *
+     * @return void
+     */
+    public function testchangeStatusWaitingToInProgress(): void
     {                                                                     
         $demand = (new Demand())->setStatus(Demand::STATUS_WAITING);                                                            
         $this->assertSame(Demand::STATUS_IN_PROGRESS, $demand->changeStatus(Demand::STATUS_IN_PROGRESS));              
     }
 
-    public function testchangeStatusWaitingToClosed()
+    /**     
+     *
+     * @return void
+     */
+    public function testchangeStatusWaitingToClosed(): void
     {                                                                     
         $demand = (new Demand())->setStatus(Demand::STATUS_WAITING);                                                    
         $this->assertSame(Demand::STATUS_CLOSED, $demand->changeStatus(Demand::STATUS_CLOSED));              
     }
 
-    public function testchangeStatusInProgressToClosed()
+    /**     
+     *
+     * @return void
+     */
+    public function testchangeStatusInProgressToClosed(): void
     {                                                                     
         $demand = (new Demand())->setStatus(Demand::STATUS_IN_PROGRESS);                                                    
         $this->assertSame(Demand::STATUS_CLOSED, $demand->changeStatus(Demand::STATUS_CLOSED));              
